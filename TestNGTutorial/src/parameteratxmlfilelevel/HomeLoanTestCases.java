9dc2420312c7f59e2d2b56d56ec96cd3ccbe6865
sureshbhabugl <sureshbhabu.gl@gmail.com>
2020-04-13 17:30:14 -0400

    updated changes with xml file paramterization

---
+++ TestNGTutorial/src/parameteratxmlfilelevel/HomeLoanTestCases.java
@@ -0,0 +1,69 @@
+package parameteratxmlfilelevel;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.beust.jcommander.Parameter;
+
+public class HomeLoanTestCases {
+
+	/*
+	 * Class Level Annotations: These are the annotations that are being used in the
+	 * class level (.java) file and it is exclusive to that class. There are 2 level
+	 * of annotations. 1. Before Class & After Class: These are executed once at
+	 * class level when the class gets loaded and executed. 2. Before Method & After
+	 * method: These are executed once before and after every method in the same
+	 * class file (.java file)
+	 * 
+	 * XML File level Annotations: These are the annotations that are executed at
+	 * the xml file level. There are 2 levels of annotations. 1) Before Test & After
+	 * Test: These are executed once before all the tests that are marked inside the
+	 * <classes/> tags that are executed in the XML file definition hierarchy. 2)
+	 * Before Suite & After Suite: These are executed once before and after all the
+	 * tests <tests></test> in the test level that are marked in the XML file
+	 * definition hierarchy.
+	 * 
+	 */
+
+	
+	@Parameters({"URL"})
+	@Test(groups = {"regression"})
+	public void paramterTest(String URL) {
+		System.out.println("-------The parameter URL from the xml file is : "+URL);
+	}
+
+	
+	@Test(groups = {"regression"})
+	public void webLoginTest() {
+		System.out.println("@Test: This test validates if the user is able to successfully login to the web interface");
+	}
+
+	@Test(groups = {"regression"})
+	public void mobileLoginTest() {
+		System.out.println(
+				"@Test: This test validates if the user is able to successfully login to the mobile interface");
+	}
+
+	@Test
+	public void RestLoginTest() {
+		System.out.println("@Test: This test validates if the user is able to successfully login using API");
+	}
+
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("******************************************Beginning of the Suite*****************************************");
+		System.out.println(
+				"@BeforeSuite: I'll be the first method to be executed before any class will be executed as I'm suite level annotation");
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println(
+				"@AfterSuite: I'll be the first method to be executed before any class will be executed as I'm suite level annotation");
+		System.out.println("******************************************End of the Suite*****************************************");
+	}
+
+}
---
+++ TestNGTutorial/src/testnghelpers/CarLoanTestCases.java
@@ -0,0 +1,64 @@
+package testnghelpers;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class CarLoanTestCases {
+	
+	/*
+	 * TestNG has the following helpers that can be added along with the annotations.
+	 * 
+	 * 1) dependsOnMethods = ({<method names>}) - This helper is used to add the test case dependency between two or more test cases
+	 * 2) enabled=true/false - This helper is used to toggle on/off a test based on the requirement.
+	 * 3) timeOut=4000 - this attribute can be set to a value (in millisecoonds) to enable timeout for a specific method. 
+	 * 
+	 */
+	
+	@Test(groups= {"smoke"})
+	public void webLoginTest() {
+		System.out.println("@Test This test validates if the user is able to successfully login to the web interface");
+		
+	}
+	
+	@Test(dependsOnMethods={"RestLoginTest"})
+	public void mobileLoginTest() {
+		System.out.println("@Test This test validates if the user is able to successfully login to the mobile interface");
+	}
+	
+	@Test
+	public void RestLoginTest() {
+		System.out.println("@Test: This test validates if the user is able to successfully login using API");
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("******************************************Beginning of the Test (XML File Level)*****************************************");
+		System.out.println("@BeforeTest: I'll be execcuted before all the tests marked in the test level in XML file");
+	}
+	
+	//Example of enabled and disabled
+	
+	@Test(enabled=true)
+	public void enabledExample() {
+		System.out.println("I'm an example of enabled helper");
+	}
+	
+	//Example of enabled and disabled
+	
+		@Test(timeOut=2000)
+		public void zTimeOutExample() throws InterruptedException {
+			System.out.println("I'm an example of enabled helper");
+			Thread.sleep(4000);
+		}
+	
+	@AfterTest
+	public void afterTest() {
+		System.out.println("@AfterTest: I'll be execcuted after all the tests marked in the test level in XML file");
+		System.out.println("@AfterTest NOTE: It will be executed only for carLoanTestCases.java file since this is test level");
+		System.out.println("******************************************End of Test (XML File Level)*****************************************");
+
+	}
+	
+
+}
